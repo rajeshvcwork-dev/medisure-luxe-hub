262b5cf839dfb1c7f9c722c60fb631d85c0eab4f
rajeshvc.workspace <rajeshvc.work@gmail.com>
2025-12-20 11:09:14 +0530

    Update Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -257,8 +257,8 @@ const Contact = () => {
                 </div>
 
                 {/* Map Placeholder */}
-                <div className="bg-muted rounded-xl overflow-hidden h-64">
-                  <iframe
+                <div className="bg-muted rounded-xl overflow-hidden h-64"
+                
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3984.0694849449837!2d101.5833!3d3.0458!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zM8KwMDInNDQuOSJOIDEwMcKwMzUnMDAuMCJF!5e0!3m2!1sen!2smy!4v1234567890"
                     width="100%"
                     height="100%"
@@ -267,7 +267,7 @@ const Contact = () => {
                     loading="lazy"
                     referrerPolicy="no-referrer-when-downgrade"
                     title="Medisure Healthcare Location"
-                  />
+                  >
                 </div>
               </div>
             </AnimatedSection>
